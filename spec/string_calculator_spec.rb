1d760dccfad3d8fbdcf0e30009dec71e4542cfbf
noriyotcp <noriyo.akita@gmail.com>
2020-07-28 21:16:01 +0900

    Implement `.add` method that add two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,15 +9,31 @@ describe 'StringCalculator' do
       end
     end
 
-    context 'given 4' do
-      it 'returns 4' do
-        expect(StringCalculator.add('4')).to eql 4
+    context 'single numbers' do
+      context 'given 4' do
+        it 'returns 4' do
+          expect(StringCalculator.add('4')).to eql 4
+        end
+      end
+
+      context 'given 10' do
+        it 'returns 10' do
+          expect(StringCalculator.add('10')).to eql 10
+        end
       end
     end
 
-    context 'given 10' do
-      it 'returns 10' do
-        expect(StringCalculator.add('10')).to eql 10
+    context 'two numbers' do
+      context "given '2,4'" do
+        it 'returns 6' do
+          expect(StringCalculator.add('2,4')).to eql 6
+        end
+      end
+
+      context "given '17,100'" do
+        it 'returns 117' do
+          expect(StringCalculator.add('17,100')).to eql 117
+        end
       end
     end
   end
