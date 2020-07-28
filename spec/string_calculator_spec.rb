e258c78240663ac789eaa1380e7760f1fd267625
noriyotcp <noriyo.akita@gmail.com>
2020-07-28 21:02:41 +0900

    Wrap with describe

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,9 +2,11 @@ require 'rspec'
 require 'string_calculator'
 
 describe 'StringCalculator' do
-  context 'given an empty string' do
-    it 'returns zero' do
-      expect(StringCalculator.add('')).to eq 0
+  describe '.add' do
+    context 'given an empty string' do
+      it 'returns zero' do
+        expect(StringCalculator.add('')).to eq 0
+      end
     end
   end
 end
